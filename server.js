23c8c7489a7b0b6487d6fc8932ae993eafce53c6
sejinyoo-hub <sejinyoo@estaid.ai>
2025-06-10 12:41:27 +0900

    Add files via upload
    Add proxy server code

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 app.get('/mintegral-data', async (req, res) => {
   const { start, end } = req.query;
   const url = `https://report-api.mintegral.com/open/api/report/v2` +
-              `?api_key=YOUR_API_KEY&start_date=${start}&end_date=${end}` +
+              `?api_key=5bdddd44355f1caf61ec8fefaf5c9d86&start_date=${start}&end_date=${end}` +
               `&timezone=Asia/Seoul&dimension=unit_id&metric=request,impression,click,est_revenue`;
 
   try {
